b4e5194ea6d5b1992d1b9a0e59e6853712bb1f12
Pratik Sahu <pratiiksahu@gmail.com>
2025-10-15 21:13:15 +0530

    Subarray Sum Brute force approach

---
+++ 1-Arrays/16_subarray_sum.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// Problem : Find the subarry with largest sum
+int subarraySumBrutForce(int a[], int n) {
+    int largestSum = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            int currSum = 0;
+            for (int k = i; k <= j; k++) {
+                currSum += a[k];
+            }
+            largestSum = max(largestSum, currSum);
+            currSum = 0;
+        }
+    }
+    return largestSum;
+}
+
+int main() {
+    int a[] = {-2, 3, 4, -1, 5, -12, 6, 1, 3};
+    int n = sizeof(a) / sizeof(int);
+    cout << subarraySumBrutForce(a, n) << endl;
+    return 0;
+}
